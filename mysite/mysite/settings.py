0260e4de2cf129241a618fdbcee9c9ce5ced2e4f
PedroCaetano4262 <pedromicaetano2010@gmail.com>
2023-05-16 17:40:43 +0100

    RamiroEGay

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -81,8 +81,15 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydatabase' ,
+        'USER': 'ricar' ,
+        'PASSWORD': '123',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'sql_mode': 'traditional',
+        }
     }
 }
 
