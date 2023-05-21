a125b31f6353ec89e3c5ad92197a2d4ced1cab0f
PedroCaetano4262 <pedromicaetano2010@gmail.com>
2023-05-21 19:45:13 +0100

    dtfg

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -9,12 +9,10 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-import environ
+
 from pathlib import Path
 
-
-env = environ.Env()
-environ.Env.read_env()  # reading .env file
+  # reading .env file
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
